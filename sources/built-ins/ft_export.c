5dfdd75fc64a90832719167c0f0a32f04f6d6bbc
Hugo Montastruc <humontas@2E2.42angouleme.fr>
2025-04-01 14:29:26 +0200

    ft_unset

--- sources/built-ins/ft_export.c
+++ sources/built-ins/ft_export.c
@@ -1,34 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_unset.c                                         :+:      :+:    :+:   */
+/*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: humontas <humontas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 13:51:29 by humontas          #+#    #+#             */
-/*   Updated: 2025/04/01 14:26:45 by humontas         ###   ########.fr       */
+/*   Created: 2025/04/01 14:23:05 by humontas          #+#    #+#             */
+/*   Updated: 2025/04/01 14:29:18 by humontas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	ft_unset(t_data *data, char **args)
-{
-	int	i;
-	int	j;
-
-	if (!args)
-		return ;
-	i = 0;
-	j = 0;
-	while (args[i])
-	{
-		while (data->envp[j])
-		{
-			if (!ft_strcmp(args[i], data->envp[j]))
-				free(data->envp[j]);
-			j++;
-		}
-		i++;
-	}
-}
+void	ft_export(char **args);
